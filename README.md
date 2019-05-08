e1c35205668e4cdcd56e836a9d44387afdb2c81c
freewind <nowindlee@gmail.com>
2019-05-08 21:12:26 +0800

    beforeAll

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ TypeScript "jasmine-expect" Demo
 
 使用jasmine-expect让jasmine有更多的matcher
 
+注意：`beforeAll`必须放在某一个`describe`中，其内部的`jasmine-expect`才能生效，否则找不到增加的那些matcher.
+
 ```
 npm install
 npm test
--- spec/hello-spec.ts
+++ spec/hello-spec.ts
@@ -1,5 +1,11 @@
 import 'jasmine-expect';
 
+describe('beforeAll must be inside "describe" to work with jasmine-expect', () => {
+  beforeAll(() => {
+    expect('hello').toBeSameLengthAs("abcde");
+  });
+});
+
 describe("A suite", function () {
   it("contains spec with an expectation", function () {
     expect('hello').toBeSameLengthAs("abcde");
